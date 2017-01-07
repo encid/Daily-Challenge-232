c0489dfd49c3b9f4a2763d437920a10aaf17c65a
encid <ryancav@gmail.com>
2017-01-07 00:50:49 -0500

    Add project files.

---
+++ Daily-Challenge-232/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge232
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words;
+
+            Console.Write("How many lines? ");
+            var input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input; i++) {
+
+            }
+        }
+    }
+}
