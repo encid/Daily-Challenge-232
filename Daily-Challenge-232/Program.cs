ee55d7369b3269835e09dd5a2a375a717c0a3e26
encid <ryancav@gmail.com>
2017-01-07 22:10:39 -0500

    commit

--- Daily-Challenge-232/Program.cs
+++ Daily-Challenge-232/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DailyChallenge232
 {
@@ -10,14 +9,22 @@ namespace DailyChallenge232
     {
         static void Main()
         {
-            string[] words;
-
+            string words = "";
             Console.Write("How many lines? ");
             var input = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < input; i++) {
-
+                words += string.Concat(Console.ReadLine().Where(ch => char.IsLetter(ch))).ToLower();
             }
+
+            if (words == string.Concat(words.Reverse())) {
+                Console.WriteLine("Palindrome!");
+            }
+            else {
+                Console.WriteLine("Not a palindrome!");
+            }
+            
+            Console.ReadKey();
         }
     }
 }
